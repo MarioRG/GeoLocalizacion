edb10e0cef0146495e2d906959dadfd048bba16a
MarioRG <mario.rios.guzman@gmail.com>
2013-07-20 17:23:04 -0500

    0.1b

--- actions.js
+++ actions.js
@@ -1,28 +1,22 @@
-$(document).ready(function(){
-  document.addEventListener("deviceready",
-                            function{
-    // onSuccess Callback
-    // This method accepts a Position object, which contains the
-    // current GPS coordinates
-    //
-    var onSuccess = function(position) {
+$(document).ready(function ()
+{
+  document.addEventListener("deviceready",function(){ 
+     var onSuccess = function(position)
+      {
         initialize(position.coords.latitude,position.coords.longitude);
-    };
-    
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
+      };
+      function onError(error) 
+      {
         alert('code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
-    }
-    
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-                                },
-                                false)
+      }
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  },
+    false
+   )
 });
 
-function initialize(lat,long) {
-				//Posición del mapa
+function initialize(lat,long){
 				var latlng = new google.maps.LatLng(lat,long);
 				var myOptions = {
 					zoom: 5,
